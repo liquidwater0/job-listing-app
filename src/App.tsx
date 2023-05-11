8e5ed4b6b01981c65c4b4e1c276f36d4a9b5ad5b
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-05-11 17:04:09 -0500

    header image

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,54 @@ import "./scss/App.scss";
 import { useJobListing } from "./context/JobListingContext";
 import Listing from "./components/Listing";
 import Filter from "./components/Filter";
+import mobileHeaderImage from "./assets/bg-header-mobile.svg";
+import desktopHeaderImage from "./assets/bg-header-desktop.svg";
 
 function App() {
 	const { renderedListings, filters, clearFilters } = useJobListing();
 
 	return (
-		<main className="main">
-			<div className="filters-container">
-				{filters.map(filter =>
-					<Filter
-						key={filter}
-						filter={filter}
-					/>	
-				)}
+		<>
+			<header>
+				<picture className='sidebar-image'>
+					<source media='(max-width: 900px)' srcSet={mobileHeaderImage}/>
+					<source media='(min-width: 1024px)' srcSet={desktopHeaderImage}/>
+					<img src={desktopHeaderImage} alt="header image" />
+				</picture>
+			</header>
 
-				<button 
-					className="clear-filters-button"
-					onClick={clearFilters}
-				>
-					Clear
-				</button>
-			</div>
+			<main className="main">
+				<div className="filters-container">
+					{filters.map(filter =>
+						<Filter
+							key={filter}
+							filter={filter}
+						/>	
+					)}
 
-			<div className="listings-container">
-				{renderedListings.map(listing =>
-					<Listing
-						key={listing.id}
-						listing={listing}
-					/>
-				)}
-			</div>
+					<button 
+						className="clear-filters-button"
+						onClick={clearFilters}
+					>
+						Clear
+					</button>
+				</div>
 
-			{/* <div className="attribution">
-				Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-				Coded by <a href="https://www.frontendmentor.io/profile/liquidwater0">@liquidwater0</a>.
-			</div> */}
-		</main>
+				<div className="listings-container">
+					{renderedListings.map(listing =>
+						<Listing
+							key={listing.id}
+							listing={listing}
+						/>
+					)}
+				</div>
+
+				{/* <div className="attribution">
+					Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+					Coded by <a href="https://www.frontendmentor.io/profile/liquidwater0">@liquidwater0</a>.
+				</div> */}
+			</main>
+		</>
 	);
 }
 
