57e0289792328db8ece408df95720ad8c3e6b025
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-05-11 15:18:58 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./scss/App.scss";
+
+function App() {
+	return (
+		<>
+			app
+
+			{/* <div className="attribution">
+				Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+				Coded by <a href="https://www.frontendmentor.io/profile/liquidwater0">@liquidwater0</a>.
+			</div> */}
+		</>
+	);
+}
+
+export default App;
