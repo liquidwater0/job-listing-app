62d731bd4360b8998c63b5eb9aebe970b3aabca4
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-05-13 16:03:14 -0500

    no work

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/job-listing-app/",
-  build: { assetsInlineLimit: 0 }
+  base: "/job-listing-app/"
 });
