9b36a257346577142fba51e68ebbff6b9b3271ee
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-05-13 16:01:40 -0500

    images please load?

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/job-listing-app/"
+  base: "/job-listing-app/",
+  build: { assetsInlineLimit: 0 }
 });
